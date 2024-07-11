0331b70196abd445adaacd82656c671e2ad64b6c
mkas020103 <mdfl020103@gmail.com>
2024-07-12 00:00:27 +0800

    inference wahaha

--- training.py
+++ training.py
@@ -2,9 +2,10 @@ from TopicSegmentation import LegalBert, ModifiedStandardDecoder
 import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+import pandas as pd
 
 # Example usage
-vocab_size = 8000
+vocab_size = 5000
 embedding_dim = 768
 num_heads = 8
 ff_dim = 512
@@ -22,19 +23,32 @@ model = tf.keras.Model(inputs=[annotated_inputs, encoder_outputs], outputs=outpu
 
 model.summary()
 
-df_text = ["John Doe a lawyer at ABC Corporation filed a lawsuit on January in New York City", 
+"""df_text = ["John Doe a lawyer at ABC Corporation filed a lawsuit on January in New York City", 
            "Mama mo a dentist at DFC Corporation filed a lawsuit on January in New York City"]
 
-ruling = ['John Doe a lawyer at ABC Corporation', 'Mama mo a dentist at DFC Corporation']
+ruling = ['John Doe a lawyer at ABC Corporation', 'Mama mo a dentist at DFC Corporation']"""
+
+df = pd.read_csv('court.csv')
+
+df = df.iloc[:,2:]
+
+df.dropna(inplace=True)
+
+court_case = df['court case'].to_list()
+ruling = df['rulings'].to_list()
+
+'''os.system('cls')
+print('type:', type(court_case))
+print('type of each element: ', type(court_case[1]))'''
     
 # Initialize the preprocessor and legal BERT
 legal_bert = LegalBert()
 
-bert_output = legal_bert.get_context_vectors(df_text)
+bert_output = legal_bert.get_context_vectors(court_case)
 
 
 # Tokenize the segments
-tokenizer = Tokenizer(num_words=8000)  # Adjust num_words according to your vocabulary size
+tokenizer = Tokenizer(num_words=5000)  # Adjust num_words according to your vocabulary size
 tokenizer.fit_on_texts(ruling)
 tokenized_segments = tokenizer.texts_to_sequences(ruling)
 
@@ -56,10 +70,10 @@ xtrain = bert_output.detach().numpy()
 ytrain = padded_segments
 
 # Compile the model
-model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate=1e-3), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate=1e-4), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 # Train
-model.fit([ytrain, xtrain], ytrain, epochs=5)
+model.fit([ytrain, xtrain], ytrain, epochs=20)
 
 model.save('seqtoseq.h5')
 
